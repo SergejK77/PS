6a4b0630e8c4ea282e18597f045ca8fb11ebbd7d
SergejK77 <73028678+SergejK77@users.noreply.github.com>
2024-07-06 17:38:31 +0200

    Add files via upload

---
+++ Remove_XBox_Packages.ps1
@@ -0,0 +1,8 @@
+﻿# Alle AppX-Pakete erhalten
+$appxPackages = Get-AppxPackage
+
+# Filtern der Pakete, die "xbox" im Namen haben
+$xboxPackages = $appxPackages | Where-Object { $_.Name -like "*xbox*" }
+
+# Entfernen der gefilterten Pakete
+$xboxPackages | ForEach-Object { Remove-AppxPackage -Package $_.PackageFullName }
