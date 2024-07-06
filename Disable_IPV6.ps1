12e803a6bffa4c5f7e4d8e31ea0b359237771672
SergejK77 <73028678+SergejK77@users.noreply.github.com>
2024-07-06 14:15:38 +0200

    Add files via upload
    Disable IPV6 at every adapter.

---
+++ Disable_IPV6.ps1
@@ -0,0 +1,7 @@
+﻿Disable ipv6
+
+Get-NetAdapter |foreach {Disable-NetAdapterBinding -InterfaceAlias $_.Name -ComponentID ms_tcpip6 }
+
+#Check
+
+Get-NetAdapter | foreach { Get-NetAdapterBinding -InterfaceAlias $_.Name -ComponentID ms_tcpip6 }
